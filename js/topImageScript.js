755bb0397fdfc923c5943284befda146ac6571e7
Sebi Cozma <sebi.sam.cozma@gmail.com>
2024-02-11 16:53:03 +0200

    stable banner main page

--- js/topImageScript.js
+++ js/topImageScript.js
@@ -3,8 +3,13 @@
     window.addEventListener(evt, function() {
         var imageHeight = document.getElementById('topImage').offsetHeight;
         var navbarHeight = document.getElementById('navbar').offsetHeight;
+        var overlayHeight = document.getElementById('overlay').offsetHeight;
         var vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-        var offset = Math.min(vh, imageHeight) - navbarHeight;
+        var offset //= Math.min(vh, imageHeight) - navbarHeight - overlayHeight;
+        if(vh < imageHeight)
+            offset = vh - navbarHeight - overlayHeight;
+        else
+            offset = imageHeight - overlayHeight;
         document.querySelector('.navbar').style.marginTop = offset + 'px';
       })
     );
